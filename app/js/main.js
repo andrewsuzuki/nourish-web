b96ab7d94da615cdb0a10236d8effd616e2ed09f
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2015-07-16 01:00:25 -0400

    change mod name back to app, readme small fix

--- app/js/main.js
+++ app/js/main.js
@@ -21,14 +21,14 @@ angular.element(document).ready(function() {
   ];
 
   // mount on window for testing
-  window.app = angular.module('nourish', requires);
+  window.app = angular.module('app', requires);
 
-  angular.module('nourish').constant('AppSettings', require('./constants'));
+  angular.module('app').constant('AppSettings', require('./constants'));
 
-  angular.module('nourish').config(require('./on_config'));
+  angular.module('app').config(require('./on_config'));
 
-  angular.module('nourish').run(require('./on_run'));
+  angular.module('app').run(require('./on_run'));
 
-  angular.bootstrap(document, ['nourish']);
+  angular.bootstrap(document, ['app']);
 
 });
