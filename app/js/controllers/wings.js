51e9a761d053b52ce9ec5cf6519d4cb442dd1597
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2015-07-20 00:45:37 -0400

    wings finder working

--- app/js/controllers/wings.js
+++ app/js/controllers/wings.js
@@ -5,11 +5,39 @@ var controllersModule = require('./_index');
 /**
  * @ngInject
  */
-function WingsCtrl() {
+function WingsCtrl($rootScope, $state, ItemService) {
 
     // ViewModel
     var vm = this;
 
+    vm.loaded = false;
+
+    vm.findHallmeals = function(hallmealsRaw) {
+        var hallmeals = [];
+        
+        hallmealsRaw.forEach(function(hm) {
+            hallmeals.push({
+                hall: ItemService.hall(hm.hall),
+                meal: ItemService.meal(hm.meal)
+            });
+        });
+
+        return hallmeals;
+    };
+
+    ItemService.allItems().then(function(items) {
+        var wings = [];
+
+        items.forEach(function(item) {
+            if (item.name.toLowerCase().indexOf('wing') !== -1) {
+                wings.push(item);
+            }
+        });
+
+        vm.items = wings;
+
+        vm.loaded = true;
+    });
 }
 
 controllersModule.controller('WingsCtrl', WingsCtrl);
